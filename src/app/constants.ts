7f5c4c7ef0573dbc901fef7b16eecda2f4fe92d9
root <root@DESKTOP-HsnMkt.localdomain>
2025-08-12 03:57:39 +0700

    mbh lh

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,53 +1,9 @@
-// Pastikan slash antar bagian URL benar
-export const API_URL = `${import.meta.env.SITE}/api/`;
+// Karena kita nggak fetch dari luar dulu, API_URL sementara nggak dipakai
+export const API_URL = `/api/`;
 
-// Kalau mau full static asset ambil dari demo aslinya
 export const REMOTE_ASSETS_BASE_URL = `https://flowbite-admin-dashboard.vercel.app`;
 
 export const SITE_TITLE = 'Flowbite Astro Admin Dashboard';
 
-export const RANDOMIZE = Boolean(import.meta.env.RANDOMIZE) || true;
-
-// NOTE: Unmapped
-// export const SIDEBAR = [
-// 	{
-// 		title: 'Getting started',
-// 		pages: [
-// 			{ title: 'Introduction' },
-// 			{ title: 'Quickstart' },
-// 			{ title: 'Build tools' },
-// 			{ title: 'License' },
-// 			{ title: 'Changelog' },
-// 		],
-// 	},
-// 	{
-// 		title: 'Customize',
-// 		pages: [
-// 			{ title: 'Configuration' },
-// 			{ title: 'Theming' },
-// 			{ title: 'Color' },
-// 			{ title: 'Icons' },
-// 			{ title: 'Optimization' },
-// 		],
-// 	},
-// 	{
-// 		title: 'Components',
-// 		pages: [
-// 			{ title: 'Alerts' },
-// 			{ title: 'Badge' },
-// 			{ title: 'Breadcrumb' },
-// 			{ title: 'Buttons' },
-// 			{ title: 'Button group' },
-// 			{ title: 'Card' },
-// 			{ title: 'Dropdowns' },
-// 			{ title: 'Forms' },
-// 			{ title: 'Typography' },
-// 			{ title: 'Modal' },
-// 			{ title: 'Navbar' },
-// 			{ title: 'Pagination' },
-// 			{ title: 'Progress' },
-// 			{ title: 'Tables' },
-// 			{ title: 'Tooltips' },
-// 		],
-// 	},
-// ];
+// Tetap biar true aja supaya UI tetap randomize kalau perlu
+export const RANDOMIZE = true;
